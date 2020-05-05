cbfea4ad9b12b0847334feb6342de5789db18584
Vorondur <Vorondur@gmail.com>
2020-05-05 00:03:20 -0500

    Finished Version 1 of Caesar Cipher

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TOP-ruby-projects
+# The Odin Project - Ruby Assignments
 A collection of ruby scripts created for The Odin Project
+
+---Finished---
+Caesar Cipher
+
+---Work in Progress---
---
+++ caesar-cipher/main.rb
@@ -0,0 +1,45 @@
+def cipher(text, amount)
+    new_text = ""
+    text.each_char { |c|
+        num = c.ord
+        if num >= 65 and num <= 90
+            num += amount
+            while num > 90
+                num -= 26
+            end
+            new_text += num.chr
+        elsif num >= 97 and num <= 122
+            num += amount
+            while num > 122
+                num -= 26
+            end
+            new_text += num.chr
+        else
+            new_text += c
+        end
+    }
+    return new_text
+end
+
+string_to_cipher = ""
+cipher_amount = "0"
+
+while string_to_cipher.length == 0 do
+    puts "\nWhat string would you like to cipher? "
+    string_to_cipher = gets.chomp
+    if string_to_cipher.length == 0
+        puts "Error! Nothing entered. Try again!"
+    end
+end
+
+until cipher_amount.to_i > 0 do
+    puts "\nHow many layers of ciphering do you want? "
+    cipher_amount = gets.chomp
+    if cipher_amount.to_i <= 0
+        puts "Error! Not a number, 0, or negative numbers not allowed!. Try again!"
+    end
+end
+
+final_string = cipher string_to_cipher, cipher_amount.to_i
+
+puts "\nYour ciphered string is: #{final_string}\n\n"
